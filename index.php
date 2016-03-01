bcb9f6bceefc6da9715e9a840c868bab02e55dde
cyrilHCD <lang.cyril@outlook.com>
2016-03-01 15:49:01 +0100

    Ajout front moche basique

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Cyril Lang: Un développeur pas comme les autres</title>
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!-- Fin CSS -->
+</head>
+<body>
+	<header>
+		<?php 
+			include("html/header.html");
+		?>
+	</header>
+	<div id="content">
+		<?php
+			include("html/content.html");
+		?>
+	</div>
+	<footer>
+		<?php
+			include("html/footer.html");
+		?>
+	</footer>
+</body>
+</html>
