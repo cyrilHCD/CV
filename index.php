1bd1d07a92ca068c6d120e643c338bf0547a25f7
cyrilHCD <lang.cyril@outlook.com>
2016-03-01 20:51:18 +0100

    Header, footer et squelette content

--- index.php
+++ index.php
@@ -2,26 +2,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Cyril Lang: Un développeur pas comme les autres</title>
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!-- CSS perso -->
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<!-- Fin CSS -->
 </head>
 <body>
-	<header>
-		<?php 
-			include("html/header.html");
-		?>
-	</header>
+	<?php 
+		include("html/header.html");
+	?>
 	<div id="content">
 		<?php
+			include("html/id_card.html");
 			include("html/content.html");
 		?>
 	</div>
-	<footer>
-		<?php
-			include("html/footer.html");
-		?>
-	</footer>
+	<?php
+		include("html/footer.html");
+	?>
 </body>
 </html>
